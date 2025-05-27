2747fc2e755b5405c8628120d9dbcfb8f48a84f2
Theomund <34360334+theomund@users.noreply.github.com>
2025-05-27 19:48:41 -0400

    feat: create application module

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Doorknob
 
+![Build](https://img.shields.io/github/actions/workflow/status/theomund/doorknob/linux.yml?style=for-the-badge&logo=linux&logoColor=white)
+![License](https://img.shields.io/github/license/theomund/doorknob?style=for-the-badge&logo=gnu&logoColor=white&color=mediumorchid)
+![Toolchain](https://img.shields.io/badge/toolchain-1.18.3-darkorchid?style=for-the-badge&logo=elixir&logoColor=white)
+![Version](https://img.shields.io/badge/version-0.1.0-blue?style=for-the-badge&logo=github&logoColor=white)
+
 Artificial intelligence companion written in Elixir.
+
+## License
+
+![AGPLv3](https://www.gnu.org/graphics/agplv3-with-text-162x68.png)
+
+This project uses the [GNU Affero General Public License v3.0](https://www.gnu.org/licenses/agpl-3.0.html) (or later).
+
+Consult the [`COPYING`](COPYING) file for more information.
---
+++ mix.exs
@@ -0,0 +1,40 @@
+# Doorknob - Artificial intelligence companion written in Elixir.
+# Copyright (C) 2025 Theomund
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+defmodule Doorknob.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :doorknob,
+      version: "0.1.0",
+      elixir: "~> 1.18.3",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Doorknob.Application, []}
+    ]
+  end
+
+  defp deps do
+    []
+  end
+end
